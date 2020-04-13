0ad514654d6e6f9f7572b581cb4b1106f6920cf3
YouLinNG <903607767@qq.com>
2020-04-13 23:24:34 +0800

    fix upload

--- src/routes/Entry/UploadDemo/index.js
+++ src/routes/Entry/UploadDemo/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, Col, Row, Icon, Upload, message, Button, Modal,BackTop} from 'antd'
+import {Card, Col, Row, Icon, Upload, message, Button, Modal,BackTop,Table,Divider} from 'antd'
 import CustomBreadcrumb from '../../../components/CustomBreadcrumb'
 import TypingCard from '../../../components/TypingCard'
 
@@ -11,9 +11,63 @@ function getBase64(img, callback) {
   reader.readAsDataURL(img);
 }
 
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  }, {
+    title: '性别',
+    dataIndex: 'sex',
+    key: 'sex',
+  }, {
+    title: '出生日期',
+    dataIndex: 'birthdate',
+    key: 'birthdate',
+  }, {
+    title: '护照号码',
+    dataIndex: 'passportnumber',
+    key: 'passportnumber',
+  }, {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+        <span>
+      <a>Action 一 {record.name}</a>
+      <Divider type="vertical"/>
+      <a>Delete</a>
+      <Divider type="vertical"/>
+      <a className="ant-dropdown-link">
+        More actions <Icon type="down"/>
+      </a>
+    </span>
+    ),
+  }]
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  }, {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  }, {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  }]
+
+
 const props = {
   name: 'file',
-  action: '//jsonplaceholder.typicode.com/posts/',
+  action: 'http://localhost:8080/File',
+
   headers: {
     authorization: 'authorization-text',
   },
@@ -27,66 +81,40 @@ const props = {
       message.error(`${info.file.name} 文件上传失败`);
     }
   },
-  defaultFileList: [{
-    uid: 1,
-    name: 'xxx.png',
-    status: 'done',
-    reponse: 'Server Error 500', // custom error message to show
-    url: 'http://www.baidu.com/xxx.png',
-  }, {
-    uid: 2,
-    name: 'yyy.png',
-    status: 'done',
-    url: 'http://www.baidu.com/yyy.png',
-  }, {
-    uid: 3,
-    name: 'zzz.png',
-    status: 'error',
-    reponse: 'Server Error 500', // custom error message to show
-    url: 'http://www.baidu.com/zzz.png',
-  }],
+
 }
 
-const props2 = {
-  name: 'file',
-  multiple: true,
-  action: '//jsonplaceholder.typicode.com/posts/',
-  onChange(info) {
-    const status = info.file.status;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-}
+
 
 class UploadDemo extends React.Component {
   state = {
     loading: false,
     previewVisible: false,
+    pictureList: [],
     previewImage: '',
-    fileList: [{
-      uid: -1,
-      name: 'xxx.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    }],
+    ocrData: [],
   }
 
   beforeUpload(file, fileList) {
-    const isJPG = file.type === 'image/jpeg'
-    if (!isJPG) {
-      message.error('只能上传JPG格式的图片');
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
+    this.setState(state => ({
+      pictureList: [...state.pictureList, file],
+    }));
+
+    const isLt2M = file.size / 1024 / 1024 < 50;
     if (!isLt2M) {
-      message.error('图片大小不超过 2MB!');
+      message.error('图片大小不超过 50MB!');
     }
-    return isJPG && isLt2M;
+    return isLt2M;
+  }
+
+  handlePicture = (fileList) => {
+
+    fetch("http://localhost:8080/Ocr",{type:'GET',data:fileList})
+        .then((res) => {
+          this.setState({
+            ocrData: res
+          })
+        })
   }
 
   handleChange = (info) => {
@@ -117,78 +145,47 @@ class UploadDemo extends React.Component {
 
   render() {
     const uploadButton = (
-      <div>
-        <Icon type="plus"/>
-        <div className="ant-upload-text">Upload</div>
-      </div>
+        <div>
+          <Icon type="plus"/>
+          <div className="ant-upload-text">Upload</div>
+        </div>
     );
-    const cardContent = `上传是将信息（网页、文字、图片、视频等）通过网页或者上传工具发布到远程服务器上的过程。
-          <ul class="card-ul">
-            <li>当需要上传一个或一些文件时</li>
-            <li>当需要展现上传的进度时</li>
-            <li>当需要使用拖拽交互时</li>
-          </ul>`
+
     return (
-      <div>
-        <CustomBreadcrumb arr={['输入', '上传']}/>
-        <TypingCard source={cardContent} height={234}/>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Card bordered={false} className='card-item' title='基本用法'>
-              <Upload {...props}>
-                <Button><Icon type="upload"/>Upload</Button>
-              </Upload>
-            </Card>
-            <Card bordered={false} style={{...styles.colItem, minHeight: 255}} title='照片墙'>
-              <Upload
-                action="//jsonplaceholder.typicode.com/posts/"
-                listType="picture-card"
-                fileList={this.state.fileList}
-                onPreview={this.handlePreview}
-                onChange={({fileList}) => this.setState({fileList})}
-              >
-                {this.state.fileList.length >= 3 ? null : uploadButton}
-              </Upload>
-              <Modal visible={this.state.previewVisible} onCancel={() => this.setState({previewVisible: false})}>
-                <img alt="example" style={{width: '100%'}} src={this.state.previewImage}/>
-              </Modal>
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card bordered={false} className='card-item' title='用户头像上传'>
-              <Row type='flex' align='middle'>
-                <Col span={8}>
-                  <Upload
-                    name="avatar"
-                    showUploadList={false}
-                    action='/upload'
-                    beforeUpload={this.beforeUpload}
-                    onChange={this.handleChange}
-                    listType="picture-card">
-                    <div>
-                      <Icon type={this.state.loading ? 'loading' : 'plus'}/>
-                      <div className="ant-upload-text">Upload</div>
-                    </div>
-                  </Upload>
-                </Col>
-                <Col span={16}>
-                  点击上传用户头像，并使用 beforeUpload 限制用户上传的图片格式和大小
-                </Col>
-              </Row>
-            </Card>
-            <Card bordered={false} className='card-item' title='拖拽上传'>
-              <Dragger {...props2}>
-                <p className="ant-upload-drag-icon">
-                  <Icon type="inbox"/>
-                </p>
-                <p className="ant-upload-text">点击或拖拽到此处完成上传</p>
-                <p className="ant-upload-hint">支持单个或批量上传。严禁上传公司数据或其他带文件</p>
-              </Dragger>
-            </Card>
-          </Col>
-        </Row>
-        <BackTop visibilityHeight={200} style={{right: 50}}/>
-      </div>
+        <div>
+          <CustomBreadcrumb arr={['输入', '上传']}/>
+          <Row gutter={16}>
+              <Card bordered={false} className='card-item' title='基本用法'>
+                <Upload {...props}>
+                  <Button><Icon type="upload"/>Upload</Button>
+                </Upload>
+              </Card>
+          </Row>
+          <Row>
+              <Card bordered={false} style={{...styles.colItem, minHeight: 255}} title='照片墙'>
+                <Upload
+                    action="http://localhost:8080/File/"
+                    listType="picture-card"
+                    fileList={this.state.fileList}
+                    onPreview={this.handlePreview}
+                    multiple={true}
+                    onChange={({fileList}) => this.setState({fileList})}
+                >
+                  {uploadButton}
+                </Upload>
+                <Modal visible={this.state.previewVisible} onCancel={() => this.setState({previewVisible: false})}>
+                  <img alt="example" style={{width: '100%'}} src={this.state.previewImage}/>
+                </Modal>
+                <Button type="danger" onClick={this.handlePicture(this.state.pictureList)}>
+                  开始识别
+                </Button>
+              </Card>
+
+          </Row>
+
+          <Table dataSource={this.state.ocrData} columns={columns} style={styles.tableStyle} />
+          <BackTop visibilityHeight={200} style={{right: 50}}/>
+        </div>
     )
   }
 }
